8810c556136de37e99ef1759617a667f82cd8f06
Dena Shink <dshink@iu.edu>
2025-04-30 13:52:20 -0400

    line of best fit

--- Visualizations.py
+++ Visualizations.py
@@ -13,6 +13,14 @@ def plot_heatmap(df):
 def scatterplot(df, x_column, y_column='Medical Cost'):
     plt.figure(figsize=(10, 6))
     plt.scatter(df[x_column], df[y_column], alpha=0.5)
+    
+    # Calculate line of best fit
+    z = np.polyfit(df[x_column], df[y_column], 1)
+    p = np.poly1d(z)
+    
+    # Add a line of best fit to the plot
+    plt.plot(df[x_column], p(df[x_column]), "r--", linewidth=2)
+    
     plt.xlabel(x_column)
     plt.ylabel(y_column)
     plt.title(f'Relationship between {x_column} and Medical Costs')
